cb734eb0b05b497034b7f566b9e53356f2037924
YogaHadiN <yoga_email@yahoo.com>
2022-03-26 12:28:13 +0700

    validate

--- app/Http/Controllers/WablasController.php
+++ app/Http/Controllers/WablasController.php
@@ -691,6 +691,11 @@ class WablasController extends Controller
 		$data = curl_exec($ch);
 		curl_close($ch);
 		return json_decode($data, true);
-
+	}
+	private function validateDate($date, $format = 'Y-m-d')
+	{
+		$d = DateTime::createFromFormat($format, $date);
+		// The Y ( 4 digits year ) returns TRUE for any integer with any number of digits so changing the comparison from == to === fixes the issue.
+		return $d && $d->format($format) === $date;
 	}
 }
