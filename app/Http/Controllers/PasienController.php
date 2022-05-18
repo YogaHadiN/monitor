a992845dedf1421c12aa946ef7a9158c7ca5782f
YogaHadiN <yoga_email@yahoo.com>
2022-05-18 10:52:37 +0700

    decrypt

--- app/Http/Controllers/PasienController.php
+++ app/Http/Controllers/PasienController.php
@@ -7,13 +7,36 @@ use Illuminate\Http\Request;
 class PasienController extends Controller
 {
     //
-    function eksklusi($id){
-        $id     = decrypt_string($id);
+    public function eksklusi($encrypted_id){
+        $id     = $this->decrypt_string($encrypted_id);
         $pasien = Pasien::where('id', $id)->first();
+        dd( $id, $encrypted_id );
         $pasien->jangan_disms = 1;
         $pasien->save();
         return view('pasien.eksklusi', compact(
             'pasien'
         ));
+    }
+
+    /**
+     * undocumented function
+     *
+     * @return void
+     */
+    private function name($param)
+    {
+        // Non-NULL Initialization Vector for decryption
+        $decryption_iv = '1234567891011121';
+          
+        $options = 0;
+        $ciphering = "AES-128-CTR";
+        // Store the decryption key
+        $decryption_key = "GeeksforGeeks";
+          
+        // Use openssl_decrypt() function to decrypt the data
+        $decryption=openssl_decrypt ($encryption, $ciphering, 
+                $decryption_key, $options, $decryption_iv);
+        // return decrypt
+        return $decryption;
     }
 }
