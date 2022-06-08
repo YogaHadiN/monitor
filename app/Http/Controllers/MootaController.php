597a6066718c7465f9e87e0a195a5fac8c261aa9
YogaHadiN <yoga_email@yahoo.com>
2022-06-08 20:17:43 +0700

    webhook moota

---
+++ app/Http/Controllers/MootaController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MootaController extends Controller
+{
+    public function webhook(){
+        Log::info('====================================')
+        Log::info('Ada uang masuk nih')
+        Log::info('====================================')
+    }
+    
+}
