76f0bc8f4d332bfac71d358df66ff79b9e751314
YogaHadiN <yoga_email@yahoo.com>
2022-08-22 15:10:23 +0700

    json

--- app/Http/Controllers/WablasController.php
+++ app/Http/Controllers/WablasController.php
@@ -739,11 +739,13 @@ class WablasController extends Controller
     }
     public function wablas2(){
         header('Content-Type: application/json');
-        $payload[] = [
-            'category' => 'button',
-            'message' => '{"buttons":["button 12","button 22","button 33"],"content":"sending button message.","footer":"footer here"}'
-        ];
+        if ( $this->message == '11111' ) {
+            $payload[] = [
+                'category' => 'button',
+                'message' => '{"buttons":["button 12","button 22","button 33"],"content":"sending button message.","footer":"footer here"}'
+            ];
 
-        echo json_encode(['data' => $payload]);
+            echo json_encode(['data' => $payload]);
+        }
     }
 }
