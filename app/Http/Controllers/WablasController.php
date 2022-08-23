d2a6ac08a9065ee7d9c2ba44ac0f509de60f6fe9
YogaHadiN <yoga_email@yahoo.com>
2022-08-23 09:58:57 +0700

    masuk

--- app/Http/Controllers/WablasController.php
+++ app/Http/Controllers/WablasController.php
@@ -649,7 +649,7 @@ class WablasController extends Controller
         Log::info("wablas2");
         header('Content-Type: application/json');
         if ( $this->message == '11111' ) {
-            Log::info("this_is_sparta");
+            Log::info("message masuk 11111");
             $payload[] = [
                 'category' => 'button',
                 'message' => '{"buttons":["button 12","button 22","button 33"],"content":"sending button message. ' . $this->message. '","footer":"footer here"}'
