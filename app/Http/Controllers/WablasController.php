a72aeb1cd4feb42ccf95a1c9c49687f5cb60b349
YogaHadiN <yoga_email@yahoo.com>
2022-09-30 10:38:25 +0700

    wwa deleted

--- app/Http/Controllers/WablasController.php
+++ app/Http/Controllers/WablasController.php
@@ -394,6 +394,7 @@ class WablasController extends Controller
 		if (
             !is_null($whatsapp_registration) &&
             !is_null( $whatsapp_registration->antrian->tanggal_lahir ) 
+            !isset( $whatsapp_registration_deleted ) 
         ) {
             $text = 'Data anda sudah kami terima. Apakah anda ingin melanjutkan atau ulangi karena ada kesalahan input data?';
             $text .= PHP_EOL;
