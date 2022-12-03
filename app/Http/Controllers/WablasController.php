959be4ab71d2e29c3f8bf9b2b71d0b2fe2fa1fab
YogaHadiN <yoga_email@yahoo.com>
2022-12-03 16:49:00 +0700

    oke

--- app/Http/Controllers/WablasController.php
+++ app/Http/Controllers/WablasController.php
@@ -1078,12 +1078,12 @@ class WablasController extends Controller
         Log::info( Input::get('phone') );
         Log::info( Input::get('messageType') );
         Log::info( Input::get('file') );
-        Log::info( Input::get('file') );
+        Log::info( Input::get('url') );
         Log::info( Input::get('message') );
         Log::info("===================");
 
-        $upload_cover = Input::get('file');
-        $extension = $upload_cover->getClientOriginalExtension();
+        /* $upload_cover = Input::get('file'); */
+        /* $extension = $upload_cover->getClientOriginalExtension(); */
 
         /* $upload_cover = Image::make($upload_cover); */
         /* $upload_cover->resize(1000, null, function ($constraint) { */
@@ -1092,20 +1092,20 @@ class WablasController extends Controller
         /* }); */
 
         //membuat nama file random + extension
-        $filename =	 'whatsapp' . '_' .  time().'.' . $extension;
+        /* $filename =	 'whatsapp' . '_' .  time().'.' . $extension; */
 
         //menyimpan bpjs_image ke folder public/img
-        $destination_path = 'whatsapp_image';
-        if (!str_ends_with('/', $destination_path)) {
-            $destination_path =  $destination_path . '/';
-        }
+        /* $destination_path = 'whatsapp_image'; */
+        /* if (!str_ends_with('/', $destination_path)) { */
+        /*     $destination_path =  $destination_path . '/'; */
+        /* } */
 
         //destinasi s3
         //
-        \Storage::disk('s3')->put($destination_path. $filename, file_get_contents($upload_cover));
-        // Mengambil file yang di upload
+        /* \Storage::disk('s3')->put($destination_path. $filename, file_get_contents($upload_cover)); */
+        /* // Mengambil file yang di upload */
 
-        /* $upload_cover->save($destination_path . '/' . $filename); */
+        /* /1* $upload_cover->save($destination_path . '/' . $filename); *1/ */
         
         //mengisi field bpjs_image di book dengan filename yang baru dibuat
         /* return $destination_path. $filename; */
