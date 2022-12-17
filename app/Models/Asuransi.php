b7c094fb1cc16b92a4c16926483338fc37bc0ff3
YogaHadiN <yoga_email@yahoo.com>
2022-12-17 23:44:40 +0700

    asuransi

--- app/Models/Asuransi.php
+++ app/Models/Asuransi.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Asuransi extends Model
 {
     use HasFactory;
+    public static function Bpjs(){
+        return Asuransi::where('tipe_asuransi_id', 5)->first();
+    }
+
+    public static function BiayaPribadi(){
+        return Asuransi::where('tipe_asuransi_id', 1)->first();
+    }
 }
