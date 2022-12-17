4c43461a828218e6c81fc008acaf3fbfc953514d
YogaHadiN <yoga_email@yahoo.com>
2022-12-17 23:55:56 +0700

    matikan main menu

--- app/Http/Controllers/WablasController.php
+++ app/Http/Controllers/WablasController.php
@@ -134,9 +134,9 @@ class WablasController extends Controller
             } else if (!is_null( $this->whatsapp_bpjs_dentist_registrations  )) {
                 return $this->registerWhatsappBpjsDentistRegistration(); //register untuk survey kesembuhan pasien
             } 
-            else {
-                return $this->createWhatsappMainMenu(); //whatsapp bot main menu
-            }
+            /* else { */
+            /*     return $this->createWhatsappMainMenu(); //whatsapp bot main menu */
+            /* } */
         }   
 	}
     /**
