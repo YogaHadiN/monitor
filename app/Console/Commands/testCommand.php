4af6995dd10cc30084e7511d722ab6e1fa8e3acd
YogaHadiN <yoga_email@yahoo.com>
2023-01-17 05:35:25 +0700

    dfsasdf

--- app/Console/Commands/testCommand.php
+++ app/Console/Commands/testCommand.php
@@ -42,7 +42,14 @@ class testCommand extends Command
      */
     public function handle()
     {
-        $this->refreshCekHarian();
+
+        $whatsapp_bot = WhatsappBot::with('staf')
+            ->where('no_telp', '6281381912803')
+            ->where('whatsapp_bot_service_id = 1 or whatsapp_bot_service_id = 2')
+            ->where('created_at', 'like', date('Y-m-d'). '%')
+            ->first();
+        dd( $whatsapp_bot );
+        /* $this->refreshCekHarian(); */
     }
     /**
      * undocumented function
