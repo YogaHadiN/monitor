0eb20a68f0fa1af0418f0df513b681a11f60b539
YogaHadiN <yoga_email@yahoo.com>
2023-01-17 12:03:13 +0700

    gu

--- app/Models/WhatsappBot.php
+++ app/Models/WhatsappBot.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class WhatsappBot extends Model
 {
     use HasFactory;
+    protected $guarded = [];
     public function staf(){
         return $this->belongsTo(Staf::class);
     }
