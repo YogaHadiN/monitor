7fb84926c614f6a0cce2aa17231fcd9288fbb665
YogaHadiN <yoga_email@yahoo.com>
2023-01-23 10:35:31 +0700

    dfasdfa

--- app/Models/ReservasiOnline.php
+++ app/Models/ReservasiOnline.php
@@ -9,4 +9,7 @@ class ReservasiOnline extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    public function registrasiPembayaran(){
+        return $this->belongsTo(RegistrasiPembayaran::class);
+    }
 }
