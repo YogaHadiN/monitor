51d10c2d95e3af56391d3e4e1d1a36b35a85df18
YogaHadiN <yoga_email@yahoo.com>
2023-01-26 11:10:15 +0700

    6

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -53,7 +53,7 @@ if (!function_exists('pesanErrorValidateNomorAsuransiBpjs')) {
          Log::info(strlen($value));
          Log::info('strlen($value) == 13');
          Log::info(strlen($value) == '13');
-         Log::info('strlen($value) == 13');
+         Log::info('strlen($value) == 6');
          Log::info(strlen($value) == 6);
          if (!strlen($value) == 13) {
              $pesan .= '_Nomor BPJS harus terdiri dari 13 angka_';
