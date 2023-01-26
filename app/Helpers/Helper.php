0c16f3b83ec63a018a48b35ec14f1803a1382204
YogaHadiN <yoga_email@yahoo.com>
2023-01-26 11:11:49 +0700

    git add .

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -46,16 +46,10 @@ if (!function_exists('pesanErrorValidateNomorAsuransiBpjs')) {
          if (!is_numeric($value)) {
              $pesan .= '_Nomor BPJS harus semuanya angka_';
          }
-         if (!is_numeric($value) && !strlen($value) == 13 ) {
+         if (!is_numeric($value) && !( strlen($value) == 13 ) ) {
              $pesan .= PHP_EOL;
          }
-         Log::info('strlen($value)');
-         Log::info(strlen($value));
-         Log::info('strlen($value) == 13');
-         Log::info(strlen($value) == '13');
-         Log::info('strlen($value) == 6');
-         Log::info(strlen($value) == 6);
-         if (!strlen($value) == 13) {
+         if (!(strlen($value) == 13)) {
              $pesan .= '_Nomor BPJS harus terdiri dari 13 angka_';
          }
          return $pesan;
