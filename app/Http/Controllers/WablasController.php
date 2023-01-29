9bedb6484bbb5566a1e0c06d639aacc6e34f4789
YogaHadiN <yoga_email@yahoo.com>
2023-01-29 22:35:40 +0700

    dfasdfadfadfa

--- app/Http/Controllers/WablasController.php
+++ app/Http/Controllers/WablasController.php
@@ -310,7 +310,7 @@ class WablasController extends Controller
 
         if ( 
             !is_null($this->whatsapp_registration) &&
-            !is_null( $this->botKirim )
+            isset( $this->botKirim )
         ) {
             $botKirim = $this->botKirim($this->whatsapp_registration);
             if ( is_null( $botKirim ) ) {
