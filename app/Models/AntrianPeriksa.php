34bef6c9c8cbde23954eb86016938de30528e93c
YogaHadiN <yoga_email@yahoo.com>
2023-01-29 15:45:25 +0700

    gambar

--- app/Models/AntrianPeriksa.php
+++ app/Models/AntrianPeriksa.php
@@ -9,4 +9,9 @@ class AntrianPeriksa extends Model
 	public function antrian(){
         return $this->morphOne(Antrian::class, 'antriable');
 	}
+    public function gambarPeriksa(){
+        return $this->morphMany(GambarPeriksa::class, 'gambarable');
+       
+    }
+    
 }
