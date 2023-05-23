987ed680b946be59a7802fe6456da55dc38e8420
YogaHadiN <yoga_email@yahoo.com>
2023-05-23 15:10:08 +0700

    dfasdfaf

--- app/Http/Controllers/WablasController.php
+++ app/Http/Controllers/WablasController.php
@@ -926,7 +926,7 @@ class WablasController extends Controller
     private function angkaPertama($pembanding) {
         $number = preg_replace("/[^0-9]/", "", $this->message);
         return !empty( $this->message ) 
-            && ( Input::get('messageType') != 'image' )
+            && ( Input::get('messageType') == 'text' )
             && ( $this->message[0] ==  $pembanding|| $number[0] ==  $pembanding);
     }
     
