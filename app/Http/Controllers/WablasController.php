e57b924139003f9ca3d0d21fc5e7dedffef5d935
YogaHadiN <yoga_email@yahoo.com>
2023-07-09 19:59:21 +0700

    ddd

--- app/Http/Controllers/WablasController.php
+++ app/Http/Controllers/WablasController.php
@@ -116,7 +116,7 @@ class WablasController extends Controller
              $payload[] = [
                 'category' => 'image',
                 'caption'  => 'caption image',
-                'urlFile'  => 'https://d3ldh8wclelidt.cloudfront.net/image/online_reservation/qr_code/A163.png'
+                'urlFile'  => 'https://d3ldh8wclelidt.cloudfront.net/image/setor_pph21/setorPph211_1685965654_JhqM5HUGUg.jpg'
              ];
 
             return response()->json([
