5273aa4c5b611c195cba3a350b68595cb1899b51
YogaHadiN <yoga_email@yahoo.com>
2023-07-09 16:50:59 +0700

    dfasdfasdfadfa

--- app/Http/Controllers/WablasController.php
+++ app/Http/Controllers/WablasController.php
@@ -2735,12 +2735,12 @@ class WablasController extends Controller
 
                     $response = $this->pesanBalasanBilaTerdaftar( $antrian, true );
 
-                    $fakeUrlFile =  'https://d3ldh8wclelidt.cloudfront.net/image/online_reservation/qr_code/A145.png';
-                    $payload[] = [
-                        'category' => 'image',
-                        'caption' => $response,
-                        'urlFile' => $fakeUrlFile
-                    ];
+                    /* $fakeUrlFile =  'https://d3ldh8wclelidt.cloudfront.net/image/online_reservation/qr_code/A145.png'; */
+                    /* $payload[] = [ */
+                    /*     'category' => 'image', */
+                    /*     'caption' => $response, */
+                    /*     'urlFile' => $fakeUrlFile */
+                    /* ]; */
 
                     $urlFile = trim( \Storage::disk('s3')->url($reservasi_online->qr_code_path_s3) );
                     $payloadReal[] = [
@@ -2749,14 +2749,12 @@ class WablasController extends Controller
                         'urlFile'  => $urlFile
                     ];
 
-                    Log::info(2752);
-                    Log::info($payload);
                     Log::info(2735);
                     Log::info($payloadReal);
 
                     return response()->json([
                         'status' => true,
-                        'data'   => $payload
+                        'data'   => $payloadReal
                     ])->header('Content-Type', 'application/json');
 
                 }
