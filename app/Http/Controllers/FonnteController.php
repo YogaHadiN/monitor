eb096cb152bf566f9cb394a177adb5eff23b38be
YogaHadiN <yoga_email@yahoo.com>
2023-07-10 11:41:01 +0700

    f

--- app/Http/Controllers/FonnteController.php
+++ app/Http/Controllers/FonnteController.php
@@ -8,21 +8,27 @@ use Log;
 class FonnteController extends Controller
 {
     public function getWebhook(){
+        Log::info('fonnte');
         Log::info(10);
     }
     public function postWebhook(){
+        Log::info('fonnte');
         Log::info(13);
     }
     public function getChaning(){
+        Log::info('fonnte');
         Log::info(17);
     }
     public function postChaning(){
+        Log::info('fonnte');
         Log::info(20);
     }
     public function getStatus(){
+        Log::info('fonnte');
         Log::info(23);
     }
     public function postStatus(){
+        Log::info('fonnte');
         Log::info(26);
     }
 }
