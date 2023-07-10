4e06a6339c8059b62cb596c61cf7219f30952734
YogaHadiN <yoga_email@yahoo.com>
2023-07-10 09:23:28 +0700

    dfasdfaf

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('wablas/webhook', [\App\Http\Controllers\WablasController::class, 'webhook']);
+Route::post('moota/webhook', [\App\Http\Controllers\MootaController::class, 'webhook']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
