1aad8e0b1aa1538640d5326fecaae1d8090f22dd
YogaHadiN <yoga_email@yahoo.com>
2023-07-10 11:28:21 +0700

    ddd

---
+++ app/Http/Controllers/FonnteController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FonnteController extends Controller
+{
+    public function getWebhook(){
+        
+    }
+    public function postWebhook(){
+        
+    }
+
+    public function getChaning(){
+        
+    }
+    public function postChaning(){
+        
+    }
+    public function getStatus(){
+        
+    }
+    public function postStatus(){
+        
+    }
+    
+}
