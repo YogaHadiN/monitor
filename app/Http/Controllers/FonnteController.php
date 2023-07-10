6c26e91f608c73dde10cb6ed581f72026cd437cf
YogaHadiN <yoga_email@yahoo.com>
2023-07-10 11:32:24 +0700

    s

--- app/Http/Controllers/FonnteController.php
+++ app/Http/Controllers/FonnteController.php
@@ -7,23 +7,21 @@ use Illuminate\Http\Request;
 class FonnteController extends Controller
 {
     public function getWebhook(){
-        
+        Log::info(10);
     }
     public function postWebhook(){
-        
+        Log::info(13);
     }
-
     public function getChaning(){
-        
+        Log::info(17);
     }
     public function postChaning(){
-        
+        Log::info(20);
     }
     public function getStatus(){
-        
+        Log::info(23);
     }
     public function postStatus(){
-        
+        Log::info(26);
     }
-    
 }
