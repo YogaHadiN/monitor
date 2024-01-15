7aa451fcc0c20c7c8f7a88ae5373af428a778d04
YogaHadiN <yoga_email@yahoo.com>
2024-01-15 21:15:24 +0700

    messsage

--- app/Http/Controllers/WablasEstetikController.php
+++ app/Http/Controllers/WablasEstetikController.php
@@ -8,9 +8,21 @@ use Log;
 class WablasEstetikController extends Controller
 {
     public function webhook(){
-        Log::info(
-            'bonjourno'
-        );
-        echo 'oke';
+        $message = 'Terima kasih telah menghubungi Klinik Jati Elok';
+        $message .= PHP_EOL;
+        $message .= 'Saat ini nomor Wa Customer Service kami telah berubah';
+        $message .= PHP_EOL;
+        $message .= 'Untuk menghubungi kami silahkan whatsapp ke nomor';
+        $message .= PHP_EOL;
+        $message .= PHP_EOL;
+        $message .= '082113781271';
+        $message .= PHP_EOL;
+        $message .= PHP_EOL;
+        $message .= 'Pesan anda sangat penting bagi kami';
+        $message .= PHP_EOL;
+        $message .= 'Dengan senang hati kami akan membantu kakak melalui nomor tersebut';
+        $message .= PHP_EOL;
+        $message .= 'Hormat kami, Tim Pelayanan CS Klinik Jati Elok';
+        echo $message;
     }
 }
