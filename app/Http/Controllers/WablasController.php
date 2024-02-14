068bf126634349aa4d9d1f255b4b44c5b70f8abe
YogaHadiN <yoga_email@yahoo.com>
2024-02-14 20:27:43 +0700

    dfsfasdf

--- app/Http/Controllers/WablasController.php
+++ app/Http/Controllers/WablasController.php
@@ -4484,7 +4484,6 @@ class WablasController extends Controller
             $message .= $waktu_tunggu_dokter;
 
             $message .= PHP_EOL;
-            $message .= 'Waktu Tunggu Obat : ' . 
             /* $message .= PHP_EOL; */
             /* $message .= $waktu_tunggu_obat; */
 
