7de524999fbffa781ce6b48abc474cbf66e0760e
YogaHadiN <yoga_email@yahoo.com>
2024-06-01 06:52:14 +0700

    dsaf

---
+++ app/Console/Commands/clearAllWhatsappBotServices.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class clearAllWhatsappBotServices extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        \App\Models\WhatsappComplaint::truncate();
+        \App\Models\WhatsappRecoveryIndex::truncate();
+        \App\Models\WhatsappMainMenu::truncate();
+        \App\Models\WhatsappBot::truncate();
+        \App\Models\WhatsappSatisfactionSurvey::truncate();
+        \App\Models\FailedTherapy::truncate();
+        \App\Models\KuesionerMenungguObat::truncate();
+        \App\Models\WhatsappBpjsDentistRegistration::truncate();
+        \App\Models\WhatsappJadwalKonsultasiInquiry::truncate();
+    }
+}
