c6ee2c997ea2757e17518174ed75e83a140238c7
YogaHadiN <yoga_email@yahoo.com>
2024-08-23 08:47:56 +0700

    dsfsaf

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,45 @@ Route::post('fonnte/webhook', [\App\Http\Controllers\FonnteController::class, 'p
 Route::post('fonnte/webhook/status', [\App\Http\Controllers\FonnteController::class, 'postStatus']);
 Route::post('fonnte/webhook/chaining', [\App\Http\Controllers\FonnteController::class, 'postChaining']);
 
+Route::get('antrian_online/bpjs/auth', [\App\Http\Controllers\AntrianOnlineController::class, 'token']);
+
+Route::get("antrian_online/bpjs/ref/poli/tanggal/{tanggal}", [
+    \App\Http\Controllers\AntrianOnlineController::class, 
+    "status_antrean"
+]);
+
+
+
+Route::group([
+    "middleware" => ["auth:api"]
+], function(){
+
+    Route::get("antrian_online/bpjs/antrean/status/{kode_poli}/{tanggalperiksa}", [
+        \App\Http\Controllers\AntrianOnlineController::class, 
+        "status_antrean"
+    ]);
+
+    Route::post("antrian_online/bpjs/antrean", [
+        \App\Http\Controllers\AntrianOnlineController::class, 
+        "ambil_antrean"
+    ]);
+
+    Route::get("antrian_online/bpjs/antrean/sisapeserta/{nomorkartu_jkn}/{kode_poli}/{tanggalperiksa}", [
+        \App\Http\Controllers\AntrianOnlineController::class, 
+        "sisa_antrean"
+    ]);
+
+    Route::post("antrian_online/bpjs/peserta", [
+        \App\Http\Controllers\AntrianOnlineController::class, 
+        "pasien_baru"
+    ]);
+
+    Route::put("antrian_online/bpjs/antrean/batal", [
+        \App\Http\Controllers\AntrianOnlineController::class, 
+        "batal_antrean"
+    ]);
+
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
