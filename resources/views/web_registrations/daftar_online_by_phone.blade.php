93ffd2be06541ecb682624f21f6771213887e14d
YogaHadiN <yoga_email@yahoo.com>
2025-02-03 16:32:32 +0700

    dfsdfaf

--- resources/views/web_registrations/daftar_online_by_phone.blade.php
+++ resources/views/web_registrations/daftar_online_by_phone.blade.php
@@ -88,11 +88,11 @@ td {
         <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
             <h4>Klinik Jati Elok</h4>
         </div>
-        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 float-right mt-10 text-right">
-            <button onclick="pglPasien([]); return false" class="btn btn-success btn-sm">
-                Aktifkan Notifikasi
-            </button>
-        </div>
+        {{-- <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 float-right mt-10 text-right"> --}}
+        {{--     <button onclick="pglPasien([]); return false" class="btn btn-success btn-sm"> --}}
+        {{--         Aktifkan Notifikasi --}}
+        {{--     </button> --}}
+        {{-- </div> --}}
     </div>
         <hr>
         <div id="message">
