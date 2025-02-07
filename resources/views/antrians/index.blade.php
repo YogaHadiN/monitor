5c793a7f2c591e5f9c67f78a7df3aae20228ecf0
YogaHadiN <yoga_email@yahoo.com>
2025-02-07 10:21:22 +0700

    ddfdfsfad

--- resources/views/antrians/index.blade.php
+++ resources/views/antrians/index.blade.php
@@ -12,14 +12,25 @@
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-    <style type="text/css" media="screen">
-.text-center {
-    text-align: center;
-    </style>
+<style type="text/css" media="screen">
+    .mt-20 {
+        margin-top: 20px;
+    }
+    .text-center {
+        text-align: center;
+    }
+</style>
 </head>
 <body class="text-center">
-    <h1>Daftar Online</h1>
-    <button>Daftar Online</button>
-    {{-- <a href='finspot:FingerspotReg;{{ $url_register }}' class='btn btn-sm btn-primary'>Register</a> --}} 
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                <h4>Klinik Jati Elok</h4>
+            </div>
+        </div>
+        <h1>Daftar Online</h1>
+        <button class="mt-20 btn btn-info btn-block">Klik Disini</button>
+        {{-- <a href='finspot:FingerspotReg;{{ $url_register }}' class='btn btn-sm btn-primary'>Register</a> --}} 
+    </div>
 </body>
 </html>
