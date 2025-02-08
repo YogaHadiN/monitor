ce31d864082054c9285531d5cf1d21a0f51791fc
YogaHadiN <yoga_email@yahoo.com>
2025-02-08 16:04:17 +0700

    dfdfsfa

--- app/Http/Controllers/WebRegistrationController.php
+++ app/Http/Controllers/WebRegistrationController.php
@@ -757,7 +757,7 @@ class WebRegistrationController extends Controller
 
         foreach ($antrians as $antrian) {
             $data[] = [
-                'nomor_antrian_terakhir' => $antrian->nomor_antrian_dipanggil,
+                'nomor_antrian_terakhir' => $antrian->ruangan->antrian->nomor_antrian,
                 'nama_ruangan'           => $antrian->ruangan->nama,
                 'nomor_antrian_anda'     => $antrian->nomor_antrian
             ];
