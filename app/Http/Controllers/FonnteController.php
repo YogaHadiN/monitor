02d8366924dda4924f1e195c177afc07d7e1f6f2
YogaHadiN <yoga_email@yahoo.com>
2025-02-13 19:46:53 +0700

    dfsdfsaf

--- app/Http/Controllers/FonnteController.php
+++ app/Http/Controllers/FonnteController.php
@@ -55,6 +55,7 @@ class FonnteController extends Controller
         header('Content-Type: application/json; charset=utf-8');
         $json      = file_get_contents('php://input');
         $data      = json_decode($json, true);
+        Log::info($data);
         $device    = $data['device'];
         $sender    = $data['sender'];
         $message   = $data['message'];
