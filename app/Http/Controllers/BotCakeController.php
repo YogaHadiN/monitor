fed88aa16f891c59db011aebd25b55dea0707df9
YogaHadiN <yoga_email@yahoo.com>
2025-02-20 11:57:03 +0700

    dfsdfa

--- app/Http/Controllers/BotCakeController.php
+++ app/Http/Controllers/BotCakeController.php
@@ -16,7 +16,7 @@ class BotCakeController extends Controller
     }
 
     public function webhook(){
-        Log::info('input botcake');
+        Log::info('Input Botcake Berhasil');
         Log::info( Input::all() );
     }
     
