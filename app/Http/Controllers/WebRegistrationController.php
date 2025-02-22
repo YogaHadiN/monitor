a6719fa0062703f8b64237dd0febc8cf92a57d27
YogaHadiN <yoga_email@yahoo.com>
2025-02-22 11:40:21 +0700

    dfsfasfd

--- app/Http/Controllers/WebRegistrationController.php
+++ app/Http/Controllers/WebRegistrationController.php
@@ -28,8 +28,7 @@ class WebRegistrationController extends Controller
         header("Access-Control-Allow-Credentials: true ");
         header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
         header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
-
-        Log::info('construc');
+        session()->put('tenant_id', 1);
     }
     
     public function daftar_online(){
