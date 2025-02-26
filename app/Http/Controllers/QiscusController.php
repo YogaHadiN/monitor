0a2c9ecad690d3fafd10ae620b8e1013ecc34ac1
YogaHadiN <yoga_email@yahoo.com>
2025-02-26 21:28:06 +0700

    dfsdfa

--- app/Http/Controllers/QiscusController.php
+++ app/Http/Controllers/QiscusController.php
@@ -4,15 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Log;
+use Input;
 
 class QiscusController extends Controller
 {
     public function webhookGet(){
+        Log::info("===================="); 
+        Log::info("QISCUS"); 
         Log::info(Input::all()); 
+        Log::info("===================="); 
     }
     public function webhook(){
-        
+        Log::info("===================="); 
+        Log::info("QISCUS"); 
         Log::info(Input::all()); 
+        Log::info("===================="); 
     }
     
 }
