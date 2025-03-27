170a14cfc2a39586109da1a5160a707d18bed27a
YogaHadiN <yoga_email@yahoo.com>
2025-03-27 17:15:15 +0700

    dfsdfa

--- app/Http/Controllers/WablasController.php
+++ app/Http/Controllers/WablasController.php
@@ -3152,16 +3152,16 @@ class WablasController extends Controller
                         $this->sendBotCake($message );
                         return false;
                     }
-                } else if (
-                    !$this->sudahAdaAntrianUntukTipeKonsultasi( $this->message )
-                ) {
-                    $tipe_konsultasi = TipeKonsultasi::find( $this->message );
-                    $message = 'Saat ini tidak ada antrian di ' . $tipe_konsultasi->tipe_konsultasi .'. Anda kami persilahkan untuk datang dan mengambil antrian secara langsung';
-                    $message .= PHP_EOL;
-                    $message .= PHP_EOL;
-                    $message .= $this->hapusAntrianWhatsappBotReservasiOnline();
-                    $this->sendBotCake($message );
-                    return false;
+                /* } else if ( */
+                /*     !$this->sudahAdaAntrianUntukTipeKonsultasi( $this->message ) */
+                /* ) { */
+                /*     $tipe_konsultasi = TipeKonsultasi::find( $this->message ); */
+                /*     $message = 'Saat ini tidak ada antrian di ' . $tipe_konsultasi->tipe_konsultasi .'. Anda kami persilahkan untuk datang dan mengambil antrian secara langsung'; */
+                /*     $message .= PHP_EOL; */
+                /*     $message .= PHP_EOL; */
+                /*     $message .= $this->hapusAntrianWhatsappBotReservasiOnline(); */
+                /*     $this->sendBotCake($message ); */
+                /*     return false; */
                 } else if (
                     $this->sudahAdaAntrianUntukTipeKonsultasi( $this->message )
                 ) {
