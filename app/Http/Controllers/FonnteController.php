3aa74c8b8a0210ba61caab1d858eda84d04f41d5
YogaHadiN <yoga_email@yahoo.com>
2025-05-25 12:24:14 +0700

    dfdsfa

--- app/Http/Controllers/FonnteController.php
+++ app/Http/Controllers/FonnteController.php
@@ -48,8 +48,39 @@ class FonnteController extends Controller
         );
 
         Log::info($data);
+        sendFonnte($sender, $reply);
 
     }
+    public function sendFonnte($target, $data) {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => "https://api.fonnte.com/send",
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "POST",
+          CURLOPT_POSTFIELDS => array(
+                'target' => $target,
+                'message' => $data['message'],
+                'url' => $data['url'],
+                'filename' => $data['filename'],
+            ),
+          CURLOPT_HTTPHEADER => array(
+            "Authorization: TOKEN"
+          ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $response;
+    }
+
     public function getChaning(){
         Log::info('fonnte');
         Log::info('getChaining');
