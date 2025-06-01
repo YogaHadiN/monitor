f0278a1077a707b3a4338a065672a8cd78e9219c
YogaHadiN <yoga_email@yahoo.com>
2025-06-01 18:30:13 +0700

    ddd

--- app/Listeners/SendNoTelpToAndroid.php
+++ app/Listeners/SendNoTelpToAndroid.php
@@ -22,7 +22,9 @@ class SendNoTelpToAndroid
         $factory = (new Factory)->withServiceAccount(storage_path('app/firebase/service-account.json'));
         $messaging = $factory->createMessaging();
 
-        $message = CloudMessage::withTarget('token', env('FCM_DEVICE_TOKEN'))
+        $dummyToken = 'eP2c6cU-QOqO9OEjI4p5cG:APA91bHX3sWzQG9KsB8YhN9x9ABc1xxLQgTFKkPZCjT4fPZQmXrPje3HbN7HzkURP23VapdQ03zDqaL5V_lKxT_q5ljAQt7fJKDdWpyY9tEoCPv5z5lj7XcXeZrLg7yOv0GxjI_xxTestToken';
+        $message = CloudMessage::withTarget('token', $dummyToken)
+        /* $message = CloudMessage::withTarget('token', env('FCM_DEVICE_TOKEN')) */
             ->withNotification(Notification::create('Kontak Baru', 'Nomor: ' . $noTelp->no_telp))
             ->withData([
                     'name'  => (string) $noTelp->id,
