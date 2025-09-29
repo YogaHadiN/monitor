4c76397523054d7f8a80d0cb7d5f362f3eb373e3
YogaHadiN <yoga_email@yahoo.com>
2025-09-29 14:04:08 +0700

    dfsdfa

--- app/Http/Controllers/WablasWebhookController.php
+++ app/Http/Controllers/WablasWebhookController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Input;
+use Log;
 
 class WablasWebhookController extends Controller
 {
     public function wablas(){
-        return 'ok';
+        Log::info('===============================');
+        Log::info('NO TELPPP');
+        Log::info( Input::all() );
+        Log::info('===============================');
     }
 }
