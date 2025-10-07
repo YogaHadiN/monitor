f34d10625dc32454272ae9f1330ce1329637d908
YogaHadiN <yoga_email@yahoo.com>
2025-10-07 19:33:33 +0700

    dfdfasd

--- app/Http/Controllers/SchedulledReservationController.php
+++ app/Http/Controllers/SchedulledReservationController.php
@@ -31,13 +31,13 @@ class SchedulledReservationController extends Controller
         $schedulled_reservation = SchedulledReservation::findOrFail($schedulled_reservation_id);
 
         // Siapkan informasi tambahan
-        $pasienNama  = $reservasi->nama ?? optional($reservasi->pasien)->nama;
-        $dokterNama  = optional($reservasi->staf)->nama_dengan_gelar;
+        $pasienNama  = $schedulled_reservation->nama ?? optional($schedulled_reservation->pasien)->nama;
+        $dokterNama  = optional($schedulled_reservation->staf)->nama_dengan_gelar;
 
         $petugas_pemeriksa = PetugasPemeriksa::whereDate('tanggal', Carbon::now())
-                                            ->where('staf_id', $reservasi->staf_id)
-                                            ->where('tipe_konsultasi_id', $reservasi->tipe_konsultasi_id)
-                                            ->where('ruangan_id', $reservasi->ruangan_id)
+                                            ->where('staf_id', $schedulled_reservation->staf_id)
+                                            ->where('tipe_konsultasi_id', $schedulled_reservation->tipe_konsultasi_id)
+                                            ->where('ruangan_id', $schedulled_reservation->ruangan_id)
                                             ->first();
 
         if (!is_null( $petugas_pemeriksa )) {
@@ -49,15 +49,15 @@ class SchedulledReservationController extends Controller
         }
 
 
-        $jam_reservasi_dihapus = Carbon::parse( $jamMulaiStr )
+        $jam_schedulled_reservation_dihapus = Carbon::parse( $jamMulaiStr )
                                             ->subMinutes(15)
                                             ->timezone('Asia/Jakarta')
                                             ->format('H:i');
 
         // === Pilihan cara tampilkan QR ===
         // a) Kalau sudah ada path QR tersimpan di DB / Storage:
-        $qrUrl = $reservasi->qrcode
-            ? Storage::disk('s3')->url($reservasi->qrcode)
+        $qrUrl = $schedulled_reservation->qrcode
+            ? Storage::disk('s3')->url($schedulled_reservation->qrcode)
             : null;
 
         // b) Atau generate langsung dari data string (ENDROID)
@@ -67,7 +67,7 @@ class SchedulledReservationController extends Controller
 
         return view('schedulled_reservations.qr-view', compact(
             'schedulled_reservation',
-            'jam_reservasi_dihapus',
+            'jam_schedulled_reservation_dihapus',
             'pasienNama',
             'dokterNama',
             'jamMulaiStr',
