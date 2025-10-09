48d5f665446558e8ee2fee739fb2aba888663aaa
YogaHadiN <yoga_email@yahoo.com>
2025-10-09 12:03:39 +0700

    dfdsfa

--- app/Http/Controllers/WablasController.php
+++ app/Http/Controllers/WablasController.php
@@ -4411,6 +4411,11 @@ class WablasController extends Controller
         /** @var \Illuminate\Support\Collection|\Illuminate\Database\Eloquent\Collection $list */
         $list = collect($this->petugas_pemeriksa_sekarang($reservasi_online));
 
+        Log::info('======================');
+        Log::info('LIST');
+        Log::info( $list );
+        Log::info('======================');
+
         // Hindari N+1 saat akses $petugas->staf (jika Eloquent collection)
         if (method_exists($list, 'loadMissing')) {
             $list->loadMissing('staf');
