4032b8813c24a93a269609cd2396cf45bebcbf7f
YogaHadiN <yoga_email@yahoo.com>
2025-12-18 17:55:08 +0700

    dfdsfasf

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 /* Route::get('wablas/webhook', [\App\Http\Controllers\BotCakeController::class, 'webhookGet']); */
-/* Route::post('wablas/webhook', [\App\Http\Controllers\WablasController::class, 'webhook']); */
+Route::get('wablas/webhook', [\App\Http\Controllers\WablasController::class, 'webhook']);
+Route::post('wablas/webhook', [\App\Http\Controllers\WablasController::class, 'webhook']);
 
 /* Route::get('qiscus/webhook', [\App\Http\Controllers\WablasController::class, 'webhookGet']); */
 /* Route::post('qiscus/webhook', [\App\Http\Controllers\WablasController::class, 'webhook']); */
