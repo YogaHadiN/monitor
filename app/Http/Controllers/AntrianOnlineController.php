c5b3a1061fae658699c2ca363ae3e9c1c7fffca0
YogaHadiN <yoga_email@yahoo.com>
2026-04-15 16:07:27 +0700

    dfdfa

--- app/Http/Controllers/AntrianOnlineController.php
+++ app/Http/Controllers/AntrianOnlineController.php
@@ -428,7 +428,7 @@ class AntrianOnlineController extends Controller
         $antrian->antriable_id          = $antrian->id;
         $antrian->pasien_id             = $this->pasien->id;
         $antrian->tanggal_lahir         = $this->pasien->tanggal_lahir;
-        $antrian->sudah_hadir_di_klinik = 0;
+        $antrian->sudah_hadir_di_klinik = 1;
         $antrian->alamat                = $this->pasien->alamat;
         $antrian->nama                  = $this->pasien->nama;
         $antrian->nomor_bpjs            = $nomorkartu;
