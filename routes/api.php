435f90d01e99376b00101e1b76224b4e503b0c6b
YogaHadiN <yoga_email@yahoo.com>
2026-04-20 18:35:57 +0700

    dfdsfa

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,6 @@ Route::post('/barantum/webhook', [BarantumWebhookController::class, 'handle']);
 Route::get('webhook/wablas', [\App\Http\Controllers\WablasWebhookController::class, 'wablas']);
 Route::post('webhook/wablas', [\App\Http\Controllers\WablasWebhookController::class, 'wablas']);
 
-Route::post('watzap/webhook', [\App\Http\Controllers\WatzapController::class, 'webhook']);
 Route::get('watzap/webhook', [\App\Http\Controllers\WatzapController::class, 'webhook']);
 
 Route::get('fonnte/webhook', [\App\Http\Controllers\FonnteController::class, 'getWebhook']);
